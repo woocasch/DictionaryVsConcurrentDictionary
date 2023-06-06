b4a483318c48453b0f2e5474a61f10b805097c35
Lukasz Nowakowski <lukasz.nowakowski@axadirect-solutions.pl>
2023-06-06 12:20:55 +0200

    Add benchmarks code.

---
+++ DictionaryVsConcurrentDictionary/Benchmarks.cs
@@ -0,0 +1,45 @@
+﻿namespace DictionaryVsConcurrentDictionary
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+
+    using BenchmarkDotNet.Attributes;
+
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+    [MemoryDiagnoser]
+    public class Benchmarks
+    {
+        private Random random;
+        
+        private Dictionary<int, Offer> dictionary;
+
+        private ConcurrentDictionary<int, Offer> concurrentDictionary;
+
+        [Params(10, 100, 1000)]
+        public int ItemsCount;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            var items = new Dictionary<int, Offer>();
+            for (var i = 0; i < this.ItemsCount; i++)
+            {
+                items.Add(i, new Offer($"Item {i}", "Body {i}", $"Comments {i}"));
+            }
+
+            this.dictionary = new Dictionary<int, Offer>(items);
+            this.concurrentDictionary = new ConcurrentDictionary<int, Offer>(items);
+            this.random = new Random(69);
+        }
+
+        [Benchmark]
+        public Offer FromDictionary() => this.dictionary[this.random.Next(this.ItemsCount)];
+
+        [Benchmark]
+        public Offer FromConcurrentDictionary() => this.concurrentDictionary[this.random.Next(this.ItemsCount)];
+    }
+}
---
+++ DictionaryVsConcurrentDictionary/Offer.cs
@@ -0,0 +1,18 @@
+﻿namespace DictionaryVsConcurrentDictionary
+{
+    public class Offer
+    {
+        public Offer(string title, string body, string comments)
+        {
+            this.Title = title;
+            this.Body = body;
+            this.Comments = comments;
+        }
+
+        public string Title { get; }
+
+        public string Body { get; }
+
+        public string Comments { get; }
+    }
+}
